e64a1a4edf40ffbb535363e8af6bdea02e9101cd
fengkx <liangkx8237@gmail.com>
2024-02-21 11:45:34 +0000

    chore: fix action

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "./src/config/init-config.ts";
 
 export default defineConfig({
   feedUrls: [
-    // "https://mikanani.me/RSS/Bangumi?bangumiId=3141&subgroupid=583",
+    // "https://mikanani.me/RSgaS/Bangumi?bangumiId=3141&subgroupid=583",
     "https://mikanani.me/RSS/Bangumi?bangumiId=3240&subgroupid=12",
   ],
   feed_concurrency: 4,
