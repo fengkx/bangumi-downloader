6f41af49e0449cef6bb20e1968b860f8175525be
fengkx <liangkx8237@gmail.com>
2024-04-12 01:19:33 +0800

    fix: config

--- config.ts
+++ config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "./src/config/init-config.ts";
 
 export default defineConfig({
   feedUrls: [
+    "https://mikanani.me/RSS/Bangumi?bangumiId=3240&subgroupid=12",
+    "https://mikanani.me/RSS/Bangumi?bangumiId=3141&subgroupid=583",
+    "https://mikanani.me/RSS/Bangumi?bangumiId=3215&subgroupid=34",
+    "https://mikanani.me/RSS/Bangumi?bangumiId=1505&subgroupid=422",
     "https://mikanani.me/RSS/Bangumi?bangumiId=3330&subgroupid=44",
   ],
   feed_concurrency: 4,
   job_concurrency: 8,
   prefer_subtitle_lang: ["zh-Hant", "zh-Hans", "ja"],
-  baseFolder: "BANGUMI_test",
+  baseFolder: "BANGUMI",
   notifier: {
     type: "telegram",
     user_id: Deno.env.get("TELEGRAM_USERID") ?? "",
