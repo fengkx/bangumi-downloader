ee4a1e6bdde7fa2d62592284cf9512ca15be4347
fengkx <liangkx8237@gmail.com>
2024-07-21 21:09:51 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     "https://mikanani.me/RSS/Bangumi?bangumiId=1505&subgroupid=422",
     "https://mikanani.me/RSS/Bangumi?bangumiId=3330&subgroupid=44",
     "https://mikanani.me/RSS/Bangumi?bangumiId=3341&subgroupid=370",
-    "https://mikanani.me/RSS/Bangumi?bangumiId=3373",
+    "https://mikanani.me/RSS/Bangumi?bangumiId=3373&subgroupid=382",
   ],
   feed_concurrency: 4,
   job_concurrency: 8,
